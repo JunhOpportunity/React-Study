80357d88ded14c5239c3c14266d00f3381ccf40a
JunHo Kim <89464762+JunhOpportunity@users.noreply.github.com>
2022-06-29 20:50:32 +0900

    useEffect & URL parameters

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import "./styles.css";
-// import Container from "react-bootstrap/Container";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button, Container, Nav, Navbar, Row, Col } from "react-bootstrap";
-// import Nav from "react-bootstrap/Nav";
-// import Navbar from "react-bootstrap/Navbar";
-// import Row from "react-bootstrap/Row";
-// import Col from "react-bootstrap/Col";
 import data from "./data.js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import Detail from "./detail.js";
 
 export default function App() {
   let [shoes] = useState(data);
   let navigate = useNavigate();
+
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
@@ -91,40 +87,12 @@ export default function App() {
                   })}
                 </Row>
               </Container>
-              <Detail />
+              <Detail shoes={data} />
             </div>
           }
         />
-        {/* <Route
-          path="/about"
-          element={
-            <div>
-              detail 페이지임
-              <Container>
-                <Row>
-                  {shoes.map(function (a, i) {
-                    return (
-                      <Col>
-                        <img
-                          src={
-                            "https://codingapple1.github.io/shop/shoes" +
-                            (i + 1) +
-                            ".jpg"
-                          }
-                          width="80%"
-                        />
-                        <h4>{shoes[i].title}</h4>
-                        <p>{shoes[i].content}</p>
-                        <p>{shoes[i].price}</p>
-                      </Col>
-                    );
-                  })}
-                </Row>
-              </Container>
-              <Detail />
-            </div>
-          }
-        /> */}
+
+        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
 
         <Route path="/about" element={<div>about 페이지임</div>}>
           <Route
@@ -141,6 +109,7 @@ export default function App() {
           <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
         </Route>
         <Route path="*" element={<div>404 NOT FOUND - 없는 페이지</div>} />
+        <Route path="/detail/:id" element={<div></div>} />
       </Routes>
     </div>
   );
@@ -151,6 +120,28 @@ function Event() {
     <div>
       <h4>무슨 내용이 들어갈까요?</h4>
       <Outlet></Outlet>
+      <inputCode />
     </div>
+  );
+}
+
+// 아래 코드가 input 한 ㅏ만들고 거기에 유저가 숫자 말고 다른거 입력할 때 마다 console.log에 출력하는 코드다.
+// 입력할 때 마다 text usestate가 바뀌는데 내가 useEffect의 두 번째 파라미터에 text를 넣었으니 text가 바뀔 때 마다
+// useEffect 안의 코드가 실행되어야 하는것이 맞다.
+// 그런데 useState와 useEffect가 오류가 뜬다. 내일 30일 해결해보자. 복습 철저히.
+
+function inputCode() {
+  let [text, textState] = useState("");
+
+  useEffect(() => {
+    console.log("그러지마세요");
+  }, [text]);
+
+  return (
+    <input
+      onChange={(e) => {
+        textState(e.target.value);
+      }}
+    ></input>
   );
 }
