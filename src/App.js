8e82e2c38eeae4cc4e72d4ab7829ea73012961e4
JunHo Kim <89464762+JunhOpportunity@users.noreply.github.com>
2022-06-30 20:48:55 +0900

    AJAX

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import data from "./data.js";
 import { useEffect, useState } from "react";
 import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import Detail from "./detail.js";
+import axios from "axios";
 
 export default function App() {
-  let [shoes] = useState(data);
+  let [shoes, setShoes] = useState(data);
   let navigate = useNavigate();
+  let [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -48,8 +50,6 @@ export default function App() {
         </Container>
       </Navbar>
 
-      <Link to="/">Home</Link>
-      <Link to="/detail">Detail</Link>
       <Routes>
         <Route
           path="/"
@@ -57,11 +57,54 @@ export default function App() {
             <div>
               Main 페이지임
               <div className="main-bg"></div>
+              <div>
+                {shoes.map(function (a, i) {
+                  return (
+                    <Col>
+                      <img
+                        src={
+                          "https://codingapple1.github.io/shop/shoes" +
+                          (i + 1) +
+                          ".jpg"
+                        }
+                        width="80%"
+                      />
+                      <h4>{shoes[i].title}</h4>
+                      <p>{shoes[i].content}</p>
+                      <p>{shoes[i].price}</p>
+                    </Col>
+                  );
+                })}
+              </div>
+              <button
+                onClick={() => {
+                  setCount(count + 1);
+                  {
+                    if (count >= 2) {
+                      alert("로딩중입니다.");
+                    } else {
+                      axios
+                        .get(
+                          "https://codingapple1.github.io/shop/data" +
+                            (count + 2) +
+                            ".json"
+                        )
+                        .then((결과) => {
+                          let newShoes = [...shoes, ...결과.data];
+                          setShoes(newShoes);
+                          console.log(shoes);
+                        });
+                    }
+                  }
+                }}
+              >
+                버튼
+              </button>
             </div>
           }
         />
 
-        <Route
+        {/* <Route
           path="/detail"
           element={
             <div>
@@ -86,14 +129,35 @@ export default function App() {
                     );
                   })}
                 </Row>
-              </Container>
-              <Detail shoes={data} />
+              </Container> */}
+
+        {/* <Container>
+                <Row>
+                  {새신발.map(function (a, i) {
+                    return (
+                      <Col>
+                        <img
+                          src={
+                            "https://codingapple1.github.io/shop/shoes" +
+                            (i + 1) +
+                            ".jpg"
+                          }
+                          width="80%"
+                        />
+                        <h4>{새신발[i].title}</h4>
+                        <p>{새신발[i].content}</p>
+                        <p>{새신발[i].price}</p>
+                      </Col>
+                    );
+                  })}
+                </Row>
+              </Container> */}
+        {/* <Detail shoes={data} />
             </div>
           }
-        />
+        /> */}
 
-        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
-
+        {/* <Route path="/detail/:id" element={<Detail shoes={shoes} />} /> */}
         <Route path="/about" element={<div>about 페이지임</div>}>
           <Route
             path="member"
@@ -109,7 +173,6 @@ export default function App() {
           <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
         </Route>
         <Route path="*" element={<div>404 NOT FOUND - 없는 페이지</div>} />
-        <Route path="/detail/:id" element={<div></div>} />
       </Routes>
     </div>
   );
@@ -125,23 +188,27 @@ function Event() {
   );
 }
 
+function Tab({ t }) {
+  [<div>내용</div>, <div>내용</div>, <div>내용</div>][t];
+}
+
 // 아래 코드가 input 한 ㅏ만들고 거기에 유저가 숫자 말고 다른거 입력할 때 마다 console.log에 출력하는 코드다.
 // 입력할 때 마다 text usestate가 바뀌는데 내가 useEffect의 두 번째 파라미터에 text를 넣었으니 text가 바뀔 때 마다
 // useEffect 안의 코드가 실행되어야 하는것이 맞다.
 // 그런데 useState와 useEffect가 오류가 뜬다. 내일 30일 해결해보자. 복습 철저히.
 
-function inputCode() {
-  let [text, textState] = useState("");
+// function inputCode() {
+//   let [text, textState] = useState("");
 
-  useEffect(() => {
-    console.log("그러지마세요");
-  }, [text]);
+//   useEffect(() => {
+//     console.log("그러지마세요");
+//   }, [text]);
 
-  return (
-    <input
-      onChange={(e) => {
-        textState(e.target.value);
-      }}
-    ></input>
-  );
-}
+//   return (
+//     <input
+//       onChange={(e) => {
+//         textState(e.target.value);
+//       }}
+//     ></input>
+//   );
+// }
--- src/detail.js
+++ src/detail.js
@@ -1,46 +1,46 @@
-import { useParams } from "react-router-dom";
-import React, { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+// import React, { useEffect, useState } from "react";
 
-function Detail(props) {
-  let [사라짐, 사라짐변경] = useState(true);
-  useEffect(() => {
-    // Detail 컴포넌트가 처음 update 될 때 이 코드 실행된다.
-    console.log("안녕");
-    setTimeout(() => {
-      사라짐변경(false);
-    }, 2000);
-  });
-  let [count, setCount] = useState(0);
+// function Detail(props) {
+//   let [사라짐, 사라짐변경] = useState(true);
+//   useEffect(() => {
+//     // Detail 컴포넌트가 처음 update 될 때 이 코드 실행된다.
+//     console.log("안녕");
+//     setTimeout(() => {
+//       사라짐변경(false);
+//     }, 2000);
+//   });
+//   let [count, setCount] = useState(0);
 
-  let { id } = useParams();
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-6">
-          <img
-            src={"https://codingapple1.github.io/shop/shoes" + id + ".jpg"}
-            width="100%"
-          />
-        </div>
-        <div className="col-md-6">
-          {/* <h4 className="pt-5">{props.shoes[id].title}</h4>
-          <p>{props.shoes[id].content}</p>
-          <p>{props.shoes[id].price}원</p> */}
-          <h4 className="pt-5">{props.shoes.find((x) => x.id == id).title}</h4>
-          <p>{props.shoes.find((x) => x.id == id).content}</p>
-          <p>{props.shoes.find((x) => x.id == id).price}원</p>
-          <button className="btn btn-danger">주문하기</button>
-          <button
-            onClick={() => {
-              setCount(count + 1);
-            }}
-          >
-            버튼
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-}
+//   let { id } = useParams();
+//   return (
+//     <div className="container">
+//       <div className="row">
+//         <div className="col-md-6">
+//           <img
+//             src={"https://codingapple1.github.io/shop/shoes" + (Number(id)+1) + ".jpg"}
+//             width="100%"
+//           />
+//         </div>
+//         <div className="col-md-6">
+//           <h4 className="pt-5">{props.shoes[id].title}</h4>
+//           <p>{props.shoes[id].content}</p>
+//           <p>{props.shoes[id].price}원</p>
+//           {/* <h4 className="pt-5">{props.shoes.find((x) => x.id == id).title}</h4>
+//           <p>{props.shoes.find((x) => x.id == id).content}</p>
+//           <p>{props.shoes.find((x) => x.id == id).price}원</p> */}
+//           <button className="btn btn-danger">주문하기</button>
+//           <button
+//             onClick={() => {
+//               setCount(count + 1);
+//             }}
+//           >
+//             버튼
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Detail;
+// export default Detail;
